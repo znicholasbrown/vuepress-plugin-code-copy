4829ded715c1793b3398ce082c6251e3391a7243
Nicholas <nicholas@prefect.io>
2019-09-10 01:04:52 -0400

    Adds an MVP with basic options and styling.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const { path } = require('@vuepress/shared-utils')
 
-module.exports = (options, context) => ({
+module.exports = (options = {}, ctx) => ({
     define: {
-        COPY_SELECTOR: options.copy_selector || 'div[class*="language-"] pre',
-        ALIGN: options.align || 'top'
+        selector: options.selector || 'div[class*="language-"] pre',
+        align: options.align || 'bottom',
+        color: options.color || '#27b1ff',
+        backgroundColor: options.backgroundColor || '#0075b8',
+        backgroundTransition: options.backgroundTransition || true,
+        successText: options.successText || 'Copied!'
     },
+    enhanceAppFiles: [path.resolve(__dirname, 'appFile.js')],
     clientRootMixin: path.resolve(__dirname, 'clientRootMixin.js')
 })
